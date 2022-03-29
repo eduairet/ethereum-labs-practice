38e069166ca927d565fc400fd3be6fb290e9018b
eduairet <hola@eduairet.com>
2022-03-28 23:30:55 -0600

    Create firstSmartContract.sol

---
+++ firstSmartContract.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.11;
+
+contract MyContract {
+    string public myString = 'Hello World';
+}
+
+// Transaction Hash: 0x7e75c23b5e329dc402a8bcab7323d40958a81818f072115500d51856ef3164a7

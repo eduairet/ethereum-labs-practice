aaaa4afdd41081a21e88b8cfc1c773c1888feda0
eduairet <hola@eduairet.com>
2022-03-29 14:21:59 -0600

    images

--- firstSmartContract.sol
+++ firstSmartContract.sol
@@ -6,4 +6,6 @@ contract MyContract {
     string public myString = 'Hello World';
 }
 
-// Transaction Hash: 0x7e75c23b5e329dc402a8bcab7323d40958a81818f072115500d51856ef3164a7
+// JavaScript VM(London) - Contract Address: 0xd9145CCE52D386f254917e481eB44e9943F39138
+// Injected Web3 Ropsten - Contract Address: 0x8346f00379d30Dc3bf7D069C96a65ec6B30ac0EF
+// Web3 Provider         - Contract Address: 0x3dc61BFDa63a4FbA5C9bB5C20a99c97cecb90a9a
